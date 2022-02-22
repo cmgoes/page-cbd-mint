b3804276ab472a14db39a33cd5c8f4d3ecc86fa0
unknown <brewerkberly@gmail.com>
2022-02-22 14:01:26 +0900

    fetch total supply has been added

--- src/pages/Mint.js
+++ src/pages/Mint.js
@@ -1,22 +1,18 @@
 import { useState, useEffect } from "react";
-// import { connectWallet, getCurrentWalletConnected } from "../utils/interact.js";
-// import WalletConnect from "@walletconnect/client";
-// import QRCodeModal from "@walletconnect/qrcode-modal";
-
 import Walletmodel from "../utils/walletmodal.js";
 
-// const alchemyKey = process.env.REACT_APP_ALCHEMY_KEY;
+const alchemyKey = process.env.REACT_APP_ALCHEMY_KEY;
 const contractABI = require("../cbd.json");
 const contractAddress = "0xf78d6e4241303DC4b743A3Ea0ae496c78176a0Eb";
-// const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
+const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
 // const web3 = createAlchemyWeb3(alchemyKey);
 // const contract = new web3.eth.Contract(contractABI, contractAddress);  
 
 export default function Mint() {
   const [walletAddress, setWallet] = useState("");  
   const [tokenNum, setTokenNum] = useState(1);
-  // const [supply, setSupply] = useState(0);
-  const supply = 0;
+  const [supply, setSupply] = useState(0);
+  // const supply = 0;
 
   const {web3Loading, getweb3} = Walletmodel ();
   const [web3, setMyWeb3] = useState ();
@@ -25,13 +21,10 @@ export default function Mint() {
       setMyWeb3 (response);      
       response.eth.getAccounts().then ((result) => {
         setWallet(result[0]);
-        console.log (web3Loading);
-        // console.log(myWeb3);
+        console.log (web3Loading);        
       });
     });
   };
-
-  // const contract = new web3.eth.Contract(contractABI, contractAddress);
 
   const mintNFT = async(amount, price, state) => {  
     // const {address} = await getCurrentWalletConnected();
@@ -65,95 +58,6 @@ export default function Mint() {
       }   
     }
   } 
-
-
-
-
-
-
-
-
-  // const [connector, setConnector] = useState({ });
-  // const [publicsaledate, setPublicsaledate] = useState(new Date("12 January 2022 16:00:00 UTC").getTime());
-  // const [timerInterval, setTimerInterval] = useState(null);
-  // const [presaleState, setPresaleState] = useState(false);
-
-  // const subscribeToEvents = () => {
-  //   // const { connector } = this.state;
-  
-  //   if (!connector) {
-  //     return;
-  //   }
-  
-  //   // When the DApp is connected to the wallet, if the wallet info changes (chainId or address in most cases), the session_update callback will be triggered
-  //   connector.on("session_update", async (error, payload) => {
-  //     console.log(`connector.on("session_update")`, payload);
-  
-  //     if (error) {
-  //       throw error;
-  //     }
-  
-  //     const { chainId, accounts } = payload.params[0];
-  
-  //     // Obtain accounts and chainId through payload.params and update the state of the DApp
-  //     this.onSessionUpdate(accounts, chainId);
-  //   });
-  
-  //   connector.on("connect", (error, payload) => {
-  //     console.log(`connector.on("connect")`, payload);
-  
-  //     if (error) {
-  //       throw error;
-  //     }
-  
-  //     this.onConnect(payload);
-  //   });
-  
-  //   connector.on("disconnect", (error, payload) => {
-  //     console.log(`connector.on("disconnect")`, payload);
-  
-  //     if (error) {
-  //       throw error;
-  //     }
-  
-  //     this.onDisconnect();
-  //   });
-  
-  //   // If the connection already exists, the QR code popup will not be triggered after walletConnectInit is executed, and you’ll only have to update the state of the DApp
-  //   if (connector.connected) {
-  //     const { chainId, accounts } = connector;
-  //     const address = accounts[0];
-  //     this.setState({
-  //       connected: true,
-  //       chainId,
-  //       accounts,
-  //       address,
-  //     });
-  //     this.onSessionUpdate(accounts, chainId);
-  //   }
-  
-  //   this.setConnector({ connector });
-  // };
-
-
-  // const walletConnect = async () => {
-  //   // bridge url
-  //   const bridge = "https://bridge.walletconnect.org";
-  
-  //   // create new connector
-  //   const connector = new WalletConnect({ bridge, qrcodeModal: QRCodeModal });
-  
-  //   await this.setConnector({ connector });
-  
-  //   // check if already connected
-  //   if (!connector.connected) {
-  //     // create new session
-  //     await connector.createSession();
-  //   }
-  
-  //   // subscribe to events
-  //   subscribeToEvents();
-  // };
 
   const presaleState = false;
 
@@ -217,10 +121,11 @@ export default function Mint() {
       // const {address} = await getCurrentWalletConnected();   
       // setWallet(address);   
       if(web3Loading) {   
-        // var contract = new web3.eth.Contract(contractABI, contractAddress);
-        // contract.methods.totalSupply().call().then((_supply) => {        
-        //   setSupply(_supply);        
-        // }).catch((err) => console.log(err))
+        const web3 = createAlchemyWeb3(alchemyKey);
+        const contract = new web3.eth.Contract(contractABI, contractAddress); 
+        contract.methods.totalSupply().call().then((_supply) => {        
+          setSupply(_supply);        
+        }).catch((err) => console.log(err))
       }
       // addWalletListener();     
       // console.log(supply);  
